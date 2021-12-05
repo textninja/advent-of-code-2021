c0be6c54f1b63bcd39db5f6a515eb1f118426fe3
Joe Taylor <joe@textninja.net>
2021-12-05 12:39:45 -0800

    Added template and wallaby js config

---
+++ _template/solution.spec.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+const { part1, part2 } = require('./solution');
+
+describe("Day {{day}}", () => {
+
+  const input = fs.readFileSync(path.join(__dirname, "sample.txt"), "utf8");
+
+  it("should work with sample data for part 1", () => {
+
+    expect(part1(input)).toEqual(1);
+
+  });
+
+
+  it("should work with sample data for part 2", () => {
+
+    expect(part2(input)).toEqual(1);
+
+  });
+
+});
