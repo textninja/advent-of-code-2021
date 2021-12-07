2f7b2995938424126dee521d3ca9f69f29abfe62
Joe Taylor <joe@textninja.net>
2021-12-06 22:48:37 -0800

    Formatted with prettier

--- src/day07/solution.js
+++ src/day07/solution.js
@@ -3,23 +3,26 @@ function part1(input) {
 }
 
 function part2(input) {
-  return minCrabCost(input, d=>(d+1)/2*d);
+  return minCrabCost(input, (d) => ((d + 1) / 2) * d);
 }
 
-function minCrabCost(input, distfn=n=>n) {
+function minCrabCost(input, distfn = (n) => n) {
   let crabs = input.trim().split(",").map(Number);
 
-  let crabCosts = range(Math.min(...crabs), Math.max(...crabs)).map((c,i,a) => {
-    return crabs.reduce((t,cc) => t+distfn(Math.abs(cc-c)), 0);
-  });
+  let crabCosts = range(Math.min(...crabs), Math.max(...crabs)).map(
+    (c, i, a) => {
+      return crabs.reduce((t, cc) => t + distfn(Math.abs(cc - c)), 0);
+    }
+  );
 
   return Math.min(...crabCosts);
 }
 
 function range(a, b) {
-  return Array.from({ length: b-a+1 }, (_,i) => a+i);
+  return Array.from({ length: b - a + 1 }, (_, i) => a + i);
 }
 
 module.exports = {
-  part1, part2
+  part1,
+  part2,
 };
